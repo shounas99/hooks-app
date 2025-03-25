0615080b3c47c5a176039c6e253c0f37e0fcbdb1
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-25 15:26:35 -0600

    feat: add useState in CounterApp component
    - implement useState for state management within hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HooksApp } from './HooksApp'
+import { CounterApp } from './01-useState/CounterApp'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    <CounterApp />
   </StrictMode>,
 )
