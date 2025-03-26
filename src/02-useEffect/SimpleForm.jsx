046ef431c0413ddf22aea07c7dab0ad44433851d
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-26 13:48:52 -0600

    feat: add useEffect with cleanup/unmount and mount

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 
+import { Message } from "./Message"
+
 export const SimpleForm = () => {
 
     const [formState, setFormState] = useState({
@@ -19,35 +21,40 @@ export const SimpleForm = () => {
     } 
 
     useEffect( () => {
-        console.log('formState changed!');
+    }, [])
+
+    useEffect( () => {
     }, [formState])
 
     useEffect( () => {
-        console.log('email changed!');
     }, [email])
 
 
-  return (
-    <>
-        <h1>Formulario Simple</h1>
-        <hr />
+    return (
+        <>
+            <h1>Formulario Simple</h1>
+            <hr />
 
-        <input 
-            type="text" 
-            className="form-control"
-            placeholder="Username"
-            name="username"
-            value={ username }
-            onChange={ onInputChange }
-        />
-        <input 
-            type="email" 
-            className="form-control mt-2"
-            placeholder="email@email.com"
-            name="email"
-            value={ email }
-            onChange={ onInputChange }
-        />
-    </>
-  )
+            <input 
+                type="text" 
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={ username }
+                onChange={ onInputChange }
+            />
+            <input 
+                type="email" 
+                className="form-control mt-2"
+                placeholder="email@email.com"
+                name="email"
+                value={ email }
+                onChange={ onInputChange }
+            />
+
+            {
+                (username === 'shounas2') && <Message />
+            }
+        </>
+    )
 }
