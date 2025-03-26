3d181b169104af9a09161cf7801b124b3d6d2044
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-26 11:43:16 -0600

    feat: useState with customHook
    - add useCounter with custom hook to ilustrate useState vs customHook useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HooksApp } from './HooksApp'
-import { CounterApp } from './01-useState/CounterApp'
+
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <HooksApp /> */}
-    <CounterApp />
-  </StrictMode>,
+    <CounterWithCustomHook />
+  </StrictMode>
 )
