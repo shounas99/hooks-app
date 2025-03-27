f54b1a348800bc6ea00e3faaa50a8c9d4324d31b
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-27 11:21:40 -0600

    feat: add useFetch and useCounter in multiple custom hook
    - connect both hooks to use in component multipleCustomHook
    
    - add new component pokemonCard to store pokemon information

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,17 +1,41 @@
-import { useFetch } from "../hooks"
+import { useCounter, useFetch } from "../hooks"
+import { LoadingMessage } from "./LoadingMessage"
+import { PokemonCard } from "./PokemonCard"
 
 export const MultipleCustomHooks = () => {
 
-    const { data, hasError, isLoading } = useFetch( `https://pokeapi.co/api/v2/pokemon/2` )
+    const { counter, decrement, increment } = useCounter(1)
+    const { data, hasError, isLoading } = useFetch( `https://pokeapi.co/api/v2/pokemon/${ counter }` )
 
     return (
         <>
             <h1>Pokemon info</h1>
             <hr />
 
-            { isLoading && <p>Cargando...</p> }
+            { isLoading 
+                ? <LoadingMessage/> 
+                : <PokemonCard 
+                    id={data.id} 
+                    name={data.name} 
+                    sprites={[
+                        data.sprites.front_default,
+                        data.sprites.front_shiny,
+                        data.sprites.back_default,
+                        data.sprites.back_shiny,
+                    ]} 
+                />
+            }
 
             <h2>{ data ?.name }</h2>
+
+            <button 
+                onClick={ () => counter > 1 ? decrement() : null}
+                className="btn btn-primary mt-2" 
+            >Anterior</button>
+            <button 
+                onClick={ () => increment()}
+                className="btn btn-primary mt-2" 
+            >Siguiente</button>
         </>
     )
 }
