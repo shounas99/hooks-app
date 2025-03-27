ec1a7db8ec952db274fb7cd05aadfa5b2380d473
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-27 12:31:39 -0600

    feat: add useRef
    - using useRef to create references between HTML elements

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,12 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   // </StrictMode>
 )
