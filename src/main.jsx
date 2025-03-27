d143d462c0948111c4f7fa48a801e6647df09f86
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-27 15:18:24 -0600

    feat: add useCallback hook
    - to memorize functions results when something changes

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,12 @@ import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
 import { Memorize } from './06-memos/Memorize'
 import { MemoHook } from './06-memos/MemoHook'
+import { CallbackHook } from './06-memos/CallbackHook'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <MemoHook />
+    <CallbackHook />
   // </StrictMode>
 )
