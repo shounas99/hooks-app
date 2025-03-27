0a40472c6e8a203ea1e01f3a9134e15583fb435f
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-27 14:50:51 -0600

    feat: add useMemo, it's an easy way to manage difficult taks

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,12 @@ import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
 import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <Memorize />
+    <MemoHook />
   // </StrictMode>
 )
