fb4a4907ec45923d04872b1bd664a14124a6b5a4
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-28 12:08:51 -0600

    feat: add memoization task using callback and memo

--- src/main.jsx
+++ src/main.jsx
@@ -12,11 +12,12 @@ import { Layout } from './05-useLayoutEffect/Layout'
 import { Memorize } from './06-memos/Memorize'
 import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <CallbackHook />
+    <Padre />
   // </StrictMode>
 )
