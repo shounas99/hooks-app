0ba31ed2747903e92abc3eb958036dfacb572481
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-04-01 14:31:49 -0600

    feat: add new components into TodoApp

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,5 +1,7 @@
 import { useReducer } from "react"
 import { todoReducer } from "./todoReducer"
+import { TodoList } from "./TodoList"
+import { TodoForm } from "./TodoForm"
 
 const initialState = [
     {  
@@ -18,46 +20,27 @@ const initialState = [
 export const TodoApp = () => {
 
     const [ todos, dispatchTodo] = useReducer( todoReducer, initialState)
+
+    const handleNewTodo = ( newTodo ) => {
+        console.log('new todooo', newTodo);
+    }
+
     return (
         <>
-            <h1>TodoApp 10,<small>pendientes: 2</small></h1>
+            <h1>TodoApp 10, <small>pendientes: 2</small></h1>
             <hr />
 
             <div className="row">
                 <div className="col-7">
-                    <ul className="list-group">
-                        {
-                            todos.map( todo => (
-                                <li key={ todo.id } className="list-group-item d-flex justify-content-between">
-                                    <span className="align-self-center">Item 1</span>
-                                    <button className="btn btn-danger">Borrar</button>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                    <TodoList todos={ todos }/>
                 </div>
 
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
                     <hr />
-                    <form action="">
-                        <input 
-                            type="text"
-                            placeholder="What do we have to do?"
-                            className="form-control"
-                        />
-                        <button
-                            type="submit"
-                            className="btn btn-outline-primary mt-1"
-                        > 
-                            Agregar
-                        </button>
-
-                    </form>
+                    <TodoForm onNewTodo={ handleNewTodo }/>
                 </div>
-
             </div>
-
         </>
     )
 }
