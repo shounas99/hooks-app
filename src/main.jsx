d2ebc8b17308f5fbf77c7a8fce91a0ec90c10c10
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-04-02 14:56:39 -0600

    feat: add components to use in context

--- src/main.jsx
+++ src/main.jsx
@@ -15,12 +15,16 @@ import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 import './08-useReducer/intro-reducer'
 import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 
 
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <TodoApp />
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>
   // </StrictMode>
 )
