90f96dd69afd6a9e67b20c5dd659418395f86faf
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-04-03 11:18:32 -0600

    feat: add useContext
    - useContext allows me to consume the context information in other components

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,8 +1,24 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+
+    const { user, setUser } = useContext( UserContext )
+    
     return (
       <>
           <h1>LoginPage</h1>
           <hr />
+
+          <pre>
+            { JSON.stringify(user, null, 3)}
+          </pre>
+
+          <button 
+            onClick={ () => setUser({ id: 123, name: 'Jonas', email: 'email@gmail.com'}) }
+            className="btn btn-primary">
+            Establecer usuario
+          </button>
       </>
     )
   }
--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,11 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-    id: 123,
-    name: 'Jonas',
-    email: 'cris@gmail.com',
-}
-
 export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState()
+
   return (
-    <UserContext.Provider value={{ hi: 'world', user: user}}>
+    <UserContext.Provider value={{ user, setUser }}>
         { children }
     </UserContext.Provider>
   )
